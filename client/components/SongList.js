389c54d07ac6076941ffafba80668be752db77d0
Ethan Nguyen <ethan@timebird.org>
2024-05-23 22:37:31 +0700

    SongList query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class SongList extends Component {
+
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li className="collection-item" key={song.id}>{song.title}</li>;
+    });
+  }
+
   render() {
+
+    if (this.props.data.loading) { return <div>Loading...</div>; }
+
     return (
-      <div>
-        <h1>Song List</h1>
-      </div>
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
     );
   }
 }
 
-export default SongList;
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`; 
+
+export default graphql(query)(SongList);
