8ace8a47f3585d6eb802d6518e5c021b012a531b
Ethan Nguyen <ethan@timebird.org>
2024-05-23 23:38:36 +0700

    Song delete mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,9 +6,18 @@ import query from "../queries/fetchSongs";
 
 class SongList extends Component {
 
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
-      return <li className="collection-item" key={song.id}>{song.title}</li>;
+    return this.props.data.songs.map(({id, title}) => {
+      return (
+        <li className="collection-item" key={id}>{title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>delete</i>
+        </li>
+      );
     });
   }
 
@@ -32,4 +41,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
