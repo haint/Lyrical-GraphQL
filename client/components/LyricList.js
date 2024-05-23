9ed520000561e5cf40aa5ff6910441293ee6d441
Ethan Nguyen <ethan@timebird.org>
2024-05-24 01:18:44 +0700

    like lyric

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import { optimistic } from "apollo-client/optimistic-data/store";
 
 class LyricList extends Component {
 
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id,
+          __typename: "LyricType",
+          likes: likes + 1
+        }
+      }
+    });
+  }
+
   renderLyric() {
-    return this.props.lyrics.map(({ id, content }) => {
-      return <li className="collection-item" key={id}>{content}</li>;
+    return this.props.lyrics.map(({ id, content, likes }) => {
+      return (<li className="collection-item" key={id}>
+        {content}
+        <div className="vote-box">
+          <i className="material-icons" onClick={() => this.onLike(id, likes)}>thumb_up</i>
+          {likes}
+        </div>
+      </li>);
     });
   }
 
@@ -17,4 +40,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
